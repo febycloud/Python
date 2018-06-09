7c8cf9857e990c82de9fff0bf40cc9927c0626d1
Nebulia <unshi@outlook.com>
2018-06-09 11:56:02 +0900

    Revert "support pip10.0"
    This reverts commit 6de8e61f4926a08ddb6fee92e102b26a62a8f472.

--- update_all.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import pip
-from subprocess import call
-from pip._internal.utils.misc import get_installed_distributions
-
-for dist in get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
