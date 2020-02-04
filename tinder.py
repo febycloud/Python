fd6cae12b9631c3365811207c9e149818f70c4f3
Fyun <fei.yun.unhi@gmail.com>
2020-02-04 00:01:07 -0500

    get update

--- tinder.py
+++ tinder.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
-from selenium import webdriver
-
-import re
-import requests
-driver = webdriver.Chrome()
-driver.get("https://tinder.com/app/recs")
-driver.find_element_by_xpath(""" //div*[@aria-label='Like'] """).click()
+import time
+import pyautogui
+import webbrowser
+url='https://tinder.com/'
+webbrowser.open(url)
+time.sleep(10)
+i=0
+for i in range(0,10):
+	i+=1
+	pyautogui.keyDown('right')
+	time.sleep(0.3)
+	pyautogui.keyUp('right')
+	time.sleep(2)
